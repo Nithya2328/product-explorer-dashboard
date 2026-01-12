64740635480865ae74794b26d968e43d60e4c382
unknown <nithyakavimarimuthu@gmail.com>
2026-01-12 11:12:29 +0530

    Changes in page.tsx for deployement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
-import { getProducts } from "../lib/api";
-import ProductExplorer from "./product-explorer";
+"use client"; // CLIENT COMPONENT
 
-export default async function HomePage() {
-  const products = await getProducts();
+import { useEffect, useState } from "react";
+import ProductExplorer from "./product-explorer";
+import { Product } from "../types/product";
+
+export default function HomePage() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+        if (!res.ok) throw new Error("Failed to fetch products");
+        const data: Product[] = await res.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  if (loading) return <p className="p-4">Loading products...</p>;
+
   return <ProductExplorer products={products} />;
 }
